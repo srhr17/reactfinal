6f7191cdc8c89f438897e1dee07f20bb394aeca2
srhr17 <46963593+srhr17@users.noreply.github.com>
2020-01-30 13:37:57 +0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Rotate from 'react-rotation';
+
+function Appr() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Rotate>
+          <img src="https://dfbkuy5licyr9.cloudfront.net/wp-content/uploads/2017/05/iStock-486724980.jpg?x91200" alt="logo" />
+        </Rotate>,
+  document.querySelector('.container')
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Appr;
