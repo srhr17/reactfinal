40e46e4b7dfe277adf02258cb9affafa19c19225
srhr17 <46963593+srhr17@users.noreply.github.com>
2020-01-30 13:37:03 +0400

    Create cri.jsx

---
+++ src/components/cri.jsx
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+class cri extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div style={{ background: 'linear-gradient(white, #ffff99)' }}>
+				<div>
+					<img
+						style={{ height: '180px', marginLeft: '500px' }}
+						src="https://globalprimenews.com/wp-content/uploads/2019/07/Screenshot_20190712_170145-1.png"
+					/>
+				</div>
+				<div style={{ display: 'inline' }}>
+					<h6 style={{ marginLeft: '550px', display: 'inline' }}>
+						C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.
+					</h6>
+
+					<p style={{ fontSize: '14px', marginLeft: '560px' }}>
+						{' '}
+						• C.R.I.'s energy efficient products are well recognized by various Government Institutions, as
+						trustworthy products for various projects across the globe to save energy.
+					</p>
+					<p style={{ fontSize: '14px', marginLeft: '560px' }}>
+						• C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency
+						Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart
+						pumps with IoT enabled control panel.
+					</p>
+					<img
+						style={{ height: '320px', width: '750px', marginLeft: '560px' }}
+						src="https://thevarsity.ca/wp-content/uploads/2019/09/ARTS-TIFF_REVIEW-COURTESY_PHOTOKINO_LORBER-IMAGE_BOOK_STILL-1080x720.jpg"
+					/>
+					<p style={{ fontSize: '12px', marginLeft: '560px' }}>
+						Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj,
+						Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of
+						Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.
+					</p>
+				</div>
+				<p style={{ marginLeft: '30px', fontSize: '15px' }}>
+					INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF
+					MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.{' '}
+				</p>
+				<img
+					style={{ height: '250px', marginLeft: '150px', width: '900px', marginBottom: '30px' }}
+					src="http://hdwarena.com/wp-content/uploads/2016/12/Color-Codes-Background.jpg"
+				/>
+				<div
+					style={{
+						backgroundColor: 'red',
+						height: '70px',
+						marginLeft: '20px',
+						marginRight: '20px'
+					}}
+				>
+					<img
+						style={{
+							height: '15px',
+							width: '15px',
+							display: 'inline',
+							marginLeft: '180px'
+						}}
+						src="https://cdn0.iconfinder.com/data/icons/market-and-economics-19/48/98-512.png"
+					/>
+					<h6
+						style={{
+							marginLeft: '2px',
+							display: 'inline',
+							color: 'white'
+						}}
+					>
+						call us
+					</h6>
+
+					<img
+						style={{
+							height: '15px',
+							width: '15px',
+							display: 'inline',
+							marginLeft: '330px'
+						}}
+						src="https://www.freepnglogos.com/uploads/logo-facebook-png/logo-facebook-facebook-logo-png-transparent-svg-vector-bie-supply-18.png"
+					/>
+					<h6
+						style={{
+							display: 'inline',
+							marginLeft: '2px',
+							color: 'white'
+						}}
+					>
+						www.facebook.com
+					</h6>
+					<img
+						style={{
+							height: '15px',
+							width: '15px',
+							display: 'inline',
+							marginLeft: '280px'
+						}}
+						src="https://cdn4.iconfinder.com/data/icons/contact-us-19/48/15-512.png"
+					/>
+					<h6
+						style={{
+							display: 'inline',
+							marginLeft: '2px',
+							color: 'white'
+						}}
+					>
+						www.crigroups.com
+					</h6>
+				</div>
+				<br />
+			</div>
+		);
+	}
+}
+
+export default cri;
